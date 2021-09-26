54241708571dc635299990b258acfb4b815f2e8b
gustavo9601 <tavo9601@gmail.com>
2021-09-25 23:34:48 -0500

    implementando signals

--- biblioteca/applications/libro/models.py
+++ biblioteca/applications/libro/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from applications.autor.models import Autor
 from .managers import LibroManager, CategoriaManager
 
+# Trigger
+from django.db.models.signals import post_save, post_delete
+
+# Pillow para imagenes
+from PIL import Image
+
 
 # Create your models here.
 class Categoria(models.Model):
@@ -43,3 +49,25 @@ class Libro(models.Model):
 
     def __str__(self):
         return f"{self.id} | {self.titulo} | {self.fecha_publicacion}"
+
+
+def optmizando_imagenes(sender, instance: Libro, **kwargs):
+    print("*" * 15)
+    print("optmizando_imagenes")
+    if instance.portada:
+        # Abrimos la imagen de la portada
+        portada = Image.open(instance.portada.path)
+        # Bajando el tamaño de la imagen, a un 20%
+        portada.save(instance.portada.path, quality=20, optimize=True)
+
+
+def alerta_eliminar_libro(sender, instance: Libro, **kwargs):
+    print("*" * 15)
+    print(f"Se elimino el libro {instance.titulo} =(")
+
+
+# Conectando el Signal (Triguer)
+# post_save => despues de crear o actualizar el modelo
+post_save.connect(optmizando_imagenes, sender=Libro)
+# post_delete => despues de eliminar
+post_delete.connect(alerta_eliminar_libro, sender=Libro)
